d9ae8e2a41076b1894bb745b2ea21214f9c83523
ducksfrogs <ma_yamaki@yahoo.com>
2023-01-29 10:53:52 +0900

    ddd

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,7 @@ import './ExpenseItem.css';
 
 function ExpenseItem() {
     return (
-        <div>
+        <div className='ExItem'>
             <div>march 28 2001</div>
             <div>
                 <h2>Car Insurance</h2>
