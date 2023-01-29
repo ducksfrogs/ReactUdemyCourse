638b7f976a40f986bd4d54fd6f81bc62eb1c16a8
ducksfrogs <ma_yamaki@yahoo.com>
2023-01-29 10:18:16 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>hello world</h2>
+      <p> this is visible</p>
     </div>
   );
 }
